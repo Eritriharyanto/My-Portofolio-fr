f9084f0be5e8ab10a13d07ad5aae0953822dd495
Eritriharyanto <wildyoung737@gmail.com>
2026-01-12 23:13:59 +0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,11 @@ export default function About() {
   return (
     <section
       id='about'
-      className='w-full min-h-screen bg-purple-700 text-white pt-32 px-10'
+      className="
+    w-full min-h-screen text-white pt-32 px-10
+    bg-[linear-gradient(rgba(88,28,135,0.85),rgba(88,28,135,0.85)),url('/src/img/Bgrafitti.png')]
+    bg-cover bg-center
+  "
     >
       {/* JUDUL */}
       <h2 className='text-4xl font-bold text-orange-400 text-center mb-10'>
@@ -25,7 +29,7 @@ export default function About() {
             Saya <span className='text-orange-300'>Eri Tri Haryanto</span>
           </h3>
 
-          <p className='text-lg leading-relaxed'>
+          <p className='text-lg leading-relaxed bg-black/50 p-6 rounded-lg'>
             Saya adalah seorang developer yang berfokus pada pengembangan web
             dan mobile. Saya memiliki minat pada UI/UX dan pengembangan aplikasi
             modern yang efisien dan menarik.
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 // src/components/Hero.jsx
-
 import React from "react";
-// import avatar from "../assets/avatar.png";
+import eth from "../img/eth.png";
 
 export default function Hero() {
   return (
@@ -10,23 +9,30 @@ export default function Hero() {
       className='w-full min-h-screen flex items-center px-10 pt-24'
     >
       {/* KIRI */}
-      <div className='w-1/2'>
+      <div className='w-1/2 max-w-[600px]'>
         <h2 className='text-3xl font-bold text-orange-500'>
           Hello, I'm <span className='text-black'>Masyarakat</span>
         </h2>
 
-        <h1 className='text-6xl font-extrabold mt-4 leading-tight'>
-          Eri Tri <br /> Haryanto
-        </h1>
+        {/* GRAFFITI NAME */}
+        <div className='mt-6'>
+          {/* ERI + TRI */}
+          <div className='flex items-end gap-3'>
+            <img src={eth} alt='eth' className='w-[300px] md:w-[800px]' />
+          </div>
+        </div>
 
-        <button className='mt-8 bg-red-500 text-white px-8 py-3 rounded-lg hover:bg-red-600'>
+        <button
+          style={{ fontFamily: "'Irish Grover', cursive" }}
+          className='mt-8 bg-red-500 mb-10 text-white px-8 py-3 rounded-lg hover:bg-red-600 '
+        >
           More
         </button>
       </div>
 
       {/* KANAN */}
-      <div className='w-1/2 flex justify-center'>
-        <div className='w-72 h-72 rounded-full overflow-hidden bg-blue-700' />
+      <div className='w-1/2 flex justify-center self-center'>
+        <div className='w-72 h-72 rounded-full bg-blue-700 -mt-6' />
       </div>
     </section>
   );
