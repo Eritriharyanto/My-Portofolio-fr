031d4dbf7e4d283ba699943328b74fbf8ceafba7
Eritriharyanto <wildyoung737@gmail.com>
2026-01-16 15:20:40 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,36 +3,33 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-      <footer className='bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4'>
+      <footer className='bg-neutral-primary-soft rounded-base shadow-xs m-4'>
         <div className='w-full max-w-screen-xl mx-auto p-4 md:py-8'>
           <div className='sm:flex sm:items-center sm:justify-between'>
             <Link
-              to='https://flowbite.com/'
-              className='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse'
+              to='#'
+              style={{ fontFamily: "'Irish Grover', cursive" }}
+              className='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse hover:text-red-700'
             >
-              <img
-                src='https://flowbite.com/docs/images/logo.svg'
-                className='h-7'
-                alt='Flowbite Logo'
-              />
-              <span className='text-heading self-center text-2xl font-semibold whitespace-nowrap'>
-                Flowbite
+              <img src='/src/img/logo.png' className='h-7' alt='Logo' />
+              <span className='text-heading self-center text-2xl font-semibold whitespace-nowrap '>
+                Eri Tri Haryanto
               </span>
             </Link>
             <ul className='flex flex-wrap items-center mb-6 text-sm font-medium text-body sm:mb-0'>
               <li>
                 <Link to='#' className='hover:underline me-4 md:me-6'>
-                  About
+                  GitHub
+                </Link>
+              </li>
+              <li>
+                <Link to='#' className='hover:underline me-4 md:me-6'>
+                  Linkind
                 </Link>
               </li>
               <li>
                 <Link to='#' className='hover:underline me-4 md:me-6'>
                   Privacy Policy
-                </Link>
-              </li>
-              <li>
-                <Link to='#' className='hover:underline me-4 md:me-6'>
-                  Licensing
                 </Link>
               </li>
               <li>
@@ -42,11 +39,11 @@ export default function Footer() {
               </li>
             </ul>
           </div>
-          <hr className='my-6 border-default sm:mx-auto lg:my-8' />
-          <span className='block text-sm text-body sm:text-center'>
-            © 2023{" "}
-            <Link to='https://flowbite.com/' className='hover:underline'>
-              Flowbite™
+          <hr className='my-6 border-default sm:mx-auto lg:my-8 text-bold' />
+          <span className='block text-bold text-sm text-body sm:text-center'>
+            © 2026{" "}
+            <Link to='#' className='hover:underline text-bold'>
+              Eri Tri Haryanto
             </Link>
             . All Rights Reserved.
           </span>
